476fb8bc0d927b8c32a67cad70272d7df9287a7d
Mick Phillips <mick.phillips@gmail.com>
2015-03-15 13:04:08 +0000

    Initial commit: geomopt.py

---
+++ geomopt.py
@@ -0,0 +1,75 @@
+from collections import namedtuple
+from numpy import tan, pi
+
+Point = namedtuple('Point', 'x, z, tanTheta')
+
+
+def zImage(rayA, rayB):
+    a = rayA.points[-2]
+    b = rayB.points[-2]
+    
+    if a.z != b.z:
+        return None
+    else:
+        return a.z + (b.x - a.x) / (a.tanTheta - b.tanTheta)
+
+
+class Surface(object):
+    def __init__(self, dz):
+        self.dz = dz
+
+
+class Lens(Surface):
+    def __init__(self, dz, f):
+        super(Lens, self).__init__(dz)
+        self.f = f
+
+
+class Pupil(Surface):
+    def __init__(self, dz, r):
+        super(Pupil, self).__init__(dz)
+        self.r = r
+
+
+class Ray(object):
+    def __init__(self, x0, tan0, col=None):
+        self.points = [ Point(float(x0), 0., float(tan0))]
+        self.redraw = [ True ]
+        if col:
+            self.colour = col
+        elif x0 == 0:
+            self.colour = 'b'
+        else:
+            self.colour = 'r'
+
+
+    def propogate(self, surfaces, start=0):
+        if start >= len(self.points):
+            start = len(self.points) - 1
+        else:
+            self.points = self.points[0:start + 1]
+        start = 0
+
+        #for i, surface in enumerate(surfaces[start:], start):
+        for i, surface in enumerate(surfaces):
+            prevPoint = self.points[i]
+            z = prevPoint.z + surface.dz
+            x = prevPoint.x + (surface.dz * prevPoint.tanTheta)
+            
+            if type(surface) is Lens:
+                # Lenses bend rays.
+                tanTheta = prevPoint.tanTheta - x / surface.f
+            else:
+                tanTheta = prevPoint.tanTheta
+            
+            self.points.append(Point(x, z, tanTheta))
+            
+            if type(surface) is Pupil:
+                if x > surface.r:
+                    break
+
+        if type(surface) is Lens:
+            zFinal = z + 2. * surface.f
+        else:
+            zFinal = z + 0.1 * surface.dz
+        self.points.append(Point(x + (zFinal - z) * tanTheta, zFinal, tanTheta))
